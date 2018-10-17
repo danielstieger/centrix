9dc2291a35bd01115e6223fb518120976572e396
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2018-10-17 16:54:46 +0200

    centrix config + runs script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -jar ./dist/centrix-1.0.0.jar -Dvertx.pool.eventloop.size=4
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+java -jar ${DIR}/dist/centrix-1.0.0.jar -Dvertx.pool.eventloop.size=4 -conf ${DIR}/centrix.conf
